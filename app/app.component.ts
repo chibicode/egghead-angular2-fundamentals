9fca3accf3ef0631a1b62d5ced7a7f8f3582605b
Shu Uesugi <shu@chibicode.com>
2016-04-02 23:06:56 -0700

    Finish lesson 18

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,14 +2,18 @@ import {Component} from 'angular2/core';
 import {TodoInput} from './todo-input';
 import {TodoList} from './todo-list';
 import {StatusSelector} from './status-selector';
+import {SearchBox} from './search-box';
 
 @Component({
   selector: 'my-app',
-  directives: [TodoInput, TodoList, StatusSelector],
+  directives: [TodoInput, TodoList, StatusSelector, SearchBox],
   template: `<div>
+    <search-box (update)='term = $event'></search-box>
     <todo-input></todo-input>
     <status-selector (select)="status = $event"></status-selector>
-    <todo-list [status]='status'></todo-list>
+    <todo-list
+      [status]='status'
+      [term]='term'></todo-list>
   </div>`
 })
 export class AppComponent { }
