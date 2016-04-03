e7db28cdce7ed83041e679d5c49fae0ff7391773
Shu Uesugi <shu@chibicode.com>
2016-04-02 21:23:58 -0700

    Finish lesson 2

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,9 @@
 import {Component} from 'angular2/core';
+import {TodoInput} from './todo-input';
 
 @Component({
-    selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+  selector: 'my-app',
+  directives: [TodoInput],
+  template: '<h1><todo-input></todo-input>My First Angular 2 App</h1>'
 })
 export class AppComponent { }
---
+++ app/todo-input.ts
@@ -0,0 +1,7 @@
+import {Component} from 'angular2/core';
+
+@Component({
+  selector: 'todo-input',
+  template: '<div>I’m a todo input</div>'
+})
+export class TodoInput {}
